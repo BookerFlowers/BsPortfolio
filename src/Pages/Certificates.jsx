e606079786a2ead10e5bfb52688e9f1a69ac1683
BookerFlowers <flowersbooker2025@gmail.com>
2025-03-12 07:53:42 -0400

    added responsiveness for mobile

--- src/Pages/Certificates.jsx
+++ src/Pages/Certificates.jsx
@@ -8,31 +8,31 @@ const Certificates = () => {
 
 
     return (
-        <div className='flex flex-col items-center justify-center  bg-black text-white  gap-8 w-full h-screen overflow-scroll p-1'>
+        <div className='flex flex-col items-center justify-center  bg-black text-white  sm:gap-8 gap-4 sm:w-screen w-full h-screen overflow-scroll sm:p-1 p-0.1'>
             <div>
                  <h1 className='font-extrabold'>Accomplishments</h1> 
-                 <img className='h-40 w-40' src="src\assets\images\bookerssuccess.png" alt="success" />
+                 <img className='sm:h-40 sm:w-40 h-20 w-20' src="src\assets\images\bookerssuccess.png" alt="success" />
 
             </div>
           
 
             <div className='flex sm:flex-row flex-col gap-5 '>
-                <img className='h-40 w-40' src="src\assets\images\codecamp.png" alt="codecamp" />
+                <img className='sm:h-40 sm:w-40 h-20 w-20' src="src\assets\images\codecamp.png" alt="codecamp" />
                
 
-                <img className='h-40 w-40' src="src\assets\images\buildingwithHTML.png" alt="html" />
+                <img className='sm:h-40 sm:w-40 h-20 w-20' src="src\assets\images\buildingwithHTML.png" alt="html" />
 
             </div>
             <div className='flex sm:flex-row flex-col gap-5 '>
 
-                <img className='h-40 w-40' src="src\assets\images\javascriptclasses.png" alt="classes" />
+                <img className='sm:h-40 sm:w-40 h-20 w-20' src="src\assets\images\javascriptclasses.png" alt="classes" />
               
-                <img className='h-40 w-40' src="src\assets\images\javascriptsecondlanguage.png" alt="secondlanguage" />
+                <img className='sm:h-40 sm:w-40 h-20 w-20' src="src\assets\images\javascriptsecondlanguage.png" alt="secondlanguage" />
             </div>
             <div className='flex sm:flex-row flex-col gap-5 '>
 
-                <img className='h-40 w-40' src="src\assets\images\javascripttricky.png" alt="jstricky" />
-                <img className='h-40 w-40' src="src\assets\images\Screenshot 2025-03-10 114117.png" alt="persevere" />
+                <img className='sm:h-40 sm:w-40 h-20 w-20' src="src\assets\images\javascripttricky.png" alt="jstricky" />
+                <img className='sm:h-40 sm:w-40 h-20 w-20' src="src\assets\images\Screenshot 2025-03-10 114117.png" alt="persevere" />
 
             </div>
 
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Projects = () => {
     return (
-        <div className='flex flex-col items-center justify-center text-white bg-black h-screen w-full overflow-scroll'>
-            <h1 className=' font-extrabold pb-20' >Projects Page</h1>
+        <div className='flex flex-col items-center justify-center text-white bg-black h-screen sm:w-screen w-full overflow-scroll'>
+            <h1 className=' font-extrabold sm:pb-20 pb-5' >Projects Page</h1>
             <div className='flex justify-center flex-col' >
-                <div className='flex sm:flex-row gap-10 p-1 flex-col'>
+                <div className='flex sm:flex-row sm:gap-10 gap-4 sm:p-1 p-0.1 flex-col'>
                     <a href="https://github.com/BookerFlowers/Functional-and-Chronological-Resume-Builder" className=' relative' >
                         <h3 className='hover:text-green-600'> Resume Builder</h3>
                         <img src="src\assets\images\resumebuilderone.png" className='w-16 h-16 rounded-full' alt="clock1" /><img src="src\assets\images\resumebuilderthree.png" className='w-16 h-16 top-9 left-9 rounded-full absolute' alt="clock2" />
@@ -13,7 +13,7 @@ const Projects = () => {
                     </a>
                     <a href="https://github.com/BookerFlowers/1st-ToDo-Drag-Drop" className='relative'>
                         <h3 className='hover:text-green-600'>ToDo App</h3>
-                        <img src="src\assets\images\firstto-do-apptwo.png" className='w-16 h-16 rounded-full' alt="todo1" /><img src="src\assets\images\firstto-do0app.png" className='w-16 h-16 top-9 left-9 rounded-full absolute' alt="todo2" />
+                        <img src="src\assets\images\firstto-do-apptwo.png" className='w-16 h-16 rounded-full' alt="todo1" /><img src="src\assets\images\firstto-do0app.png" className='w-16 h-16 top-9 left-9  rounded-full absolute' alt="todo2" />
 
                     </a>
                     <a href="https://github.com/BookerFlowers/College-Football-Cards" className='relative'>
@@ -25,7 +25,7 @@ const Projects = () => {
                 <div className='flex sm:flex-row gap-10 p-1 flex-col'>
                     <a href="https://github.com/BookerFlowers/Booker-Cruises" className='relative'>
                         <h3 className='hover:text-green-600'>Booker's Cruise</h3>
-                        <img src="src\assets\images\cruise4.png" className='w-16 h-16 rounded-full' alt="cruise1" /><img src="src\assets\images\cruise5.png" className='w-16 h-16 top-9 left-9 rounded-full absolute' alt="cruise2" />
+                        <img src="src\assets\images\cruise4.png" className='w-16 h-16 rounded-full' alt="cruise1" /><img src="src\assets\images\cruise5.png" className='w-16 h-16 top-9 left-9  rounded-full absolute' alt="cruise2" />
 
                     </a>
                     <a href="https://github.com/BookerFlowers/BONES-and-Manners-Adoption" className='relative'>
@@ -38,18 +38,18 @@ const Projects = () => {
 
                     <a href="https://github.com/BookerFlowers/rock-paper-scissors-master" className='relative'>
                         <h3 className='hover:text-green-600'>Rock, Paper, Scissors</h3>
-                        <img src="src\assets\images\r-p-s-gamefive.png" className='w-16 h-16 rounded-full' alt="rps1" /><img src="src\assets\images\r-p-s-gamefour.png" className='w-16 h-16 top-9 left-9 rounded-full absolute' alt="rps2" />
+                        <img src="src\assets\images\r-p-s-gamefive.png" className='w-16 h-16 rounded-full' alt="rps1" /><img src="src\assets\images\r-p-s-gamefour.png" className='w-16 h-16 top-9 left-9  rounded-full absolute' alt="rps2" />
 
                     </a>
                     <a href="https://github.com/BookerFlowers/Adventure-Text-Node-Game" className='relative'>
                         <h3 className='hover:text-green-600'>Adventure Game</h3>
-                        <img src="src\assets\images\adventuregamefive.png" className='w-16 h-16 rounded-full' alt="tn1" /><img src="src\assets\images\rpg-dragonrepeller2.png" className='w-16 h-16 top-9 left-9 rounded-full absolute' alt="tn2" />
+                        <img src="src\assets\images\adventuregamefive.png" className='w-16 h-16 rounded-full' alt="tn1" /><img src="src\assets\images\rpg-dragonrepeller2.png" className='w-16 h-16 top-9 left-9  rounded-full absolute' alt="tn2" />
 
                     </a>
 
                     <a href="https://github.com/BookerFlowers/Bookers-Super-Store" className='relative'>
                         <h3 className='hover:text-green-600'>Super Store</h3>
-                        <img src="src\assets\images\superstoreone.png" className='w-16 h-16 rounded-full' alt="store1" /><img src="src\assets\images\superstoreshopping.png" className='w-16 h-16 top-9 left-9 rounded-full absolute' alt="store2" />
+                        <img src="src\assets\images\superstoreone.png" className='w-16 h-16 rounded-full' alt="store1" /><img src="src\assets\images\superstoreshopping.png" className='w-16 h-16 top-9 left-9  rounded-full absolute' alt="store2" />
                     </a>
                 </div>
 
